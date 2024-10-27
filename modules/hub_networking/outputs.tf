97f91e19629136fe89f17b9fffc80df021d7399f
K.Miszel <k.miszel@protonmail.com>
2024-10-27 20:23:15 +0100

    add_hub

--- modules/hub_networking/outputs.tf
+++ modules/hub_networking/outputs.tf
@@ -8,7 +8,7 @@ output "nsg_ids" {
   value       = { for k, v in azurerm_network_security_group.nsgs : k => v.id }
 }
 
-output "subnet_ids" {
-  description = "Map of subnet IDs by subnet name."
-  value       = { for name, subnet in azurerm_subnet.hub_subnets : name => subnet.id }
-}
+# output "subnet_ids" {
+#   description = "Map of subnet IDs by subnet name."
+#   value       = { for name, subnet in azurerm_subnet.hub_subnets : name => subnet.id }
+# }
